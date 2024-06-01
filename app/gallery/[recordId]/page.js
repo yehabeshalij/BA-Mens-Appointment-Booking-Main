23d153fd2aef90beae043353f32802714500d0de
Alexo <habeshawialex@gmail.com>
2024-06-02 02:32:38 +0300

    first commit

---
+++ app/gallery/[recordId]/page.js
@@ -0,0 +1,37 @@
+// "use client";
+
+// import GlobalApi from "@/app/_utils/GlobalApi";
+// import React, { useEffect, useState } from "react";
+// import GalleryPage from "../_components/GalleryPage";
+// import GalleryPageone from "../gallerypageone/GalleryPageone";
+// import GalleryPagetwo from "../gallerypagetwo/GalleryPagetwo";
+// // import GalleryPageone from "../GalleryPageone";
+// // import GalleryPagetwo from "../GalleryPagetwo";
+
+// // import GalleryPage from "../../gallery/GalleryPage";
+// // import { GalleryPage } from "../_components/GalleryPage";
+
+// function Gallery({ params }) {
+//   // const [gallery, setGallery] = useState();
+
+//   // useEffect(() => {
+//   //   getGalleryById();
+//   // }, []);
+//   // const getGalleryById = () => {
+//   //   GlobalApi.getGalleryById(params.recordId).then((resp) => {
+//   //     setGallery(resp.data.data);
+//   //   });
+//   // };
+//   return (
+//     <div className="p-5 md:px-10">
+//       <h2 className="font-bold text-[22px] text-center">Gallery</h2>
+//       <div>
+//         <GalleryPage />
+//         {/* <GalleryPageone />
+//         <GalleryPagetwo /> */}
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default Gallery;
